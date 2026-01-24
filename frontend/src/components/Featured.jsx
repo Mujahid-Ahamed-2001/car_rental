9244d27cce771e9b0a5d1c6b8353d6209b0bb4cc
Mujahid-Ahamed-2001 <mujahidmjhd2001@gmail.com>
2026-01-24 16:49:49 +0530

    featured

--- frontend/src/components/Featured.jsx
+++ frontend/src/components/Featured.jsx
@@ -2,8 +2,10 @@ import React from 'react'
 import CarCard from './CarCard'
 import { assets, dummyCarData } from '../assets/assets'
 import Title from './Title'
+import { useNavigate } from 'react-router-dom'
 
 const Featured = () => {
+  const navigate = useNavigate();
   return (
     <div className='flex flex-col items-center py-24 px-6 md:px-16 lg:px-24 xl:px-32'>
       <div>
@@ -16,7 +18,9 @@ const Featured = () => {
           </div>
         ))}  
       </div>
-      <button className='flex items-center justify-center gap-2 px-6 py-2 border border-borderColor hover:bg-primary hover:text-white hover:scale-110 rounded-md mt-18 cursor-pointer group transition-all duration-300'>
+      <button onClick={()=>{
+        navigate('/cars'); scrollTo(0,0);
+      }} className='flex items-center justify-center gap-2 px-6 py-2 border border-borderColor hover:bg-primary hover:text-white hover:scale-110 rounded-md mt-18 cursor-pointer group transition-all duration-300'>
         Explore More <img src={assets.arrow_icon} alt="arrow" className='group-hover:translate-x-2 transition-all duration-300' />
       </button>
     </div>
